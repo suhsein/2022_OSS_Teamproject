edc720c727ba3d15e57d4a735922b51ce4295499
suhsein <mini9075@gmail.com>
2022-11-10 16:41:36 +0900

    fixed error

--- square.js
+++ square.js
@@ -1,8 +1,7 @@
-var square = function(rtm, text, channel){
-	console.log('제곱을 실시합니다.');
-	console.log(text);
-	rtm.sendMessage('The result is '+text*text,channel);
-}
+const square = function (rtm, text, channel) {
+  console.log('제곱을 실시합니다.');
+  console.log(text);
+  rtm.sendMessage(`The result is ${text * text}`, channel);
+};
 
-module.exports =square;
-
+module.exports = square;
