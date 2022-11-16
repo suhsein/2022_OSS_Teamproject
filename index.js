a71b2ebfb1c1049c4fd7125036b0eeee92371d1d
Jinnyzinny <ddocdoli@naver.com>
2022-11-16 16:34:31 +0900

    First commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { RTMClient } = require('@slack/rtm-api');
 
 let token;
+const fs = require('fs');
 
 try {
   token = fs.readFileSync('./token').toString('utf-8');
@@ -19,17 +20,23 @@ rtm.start();
 const greeting = require('./greeting');
 const square = require('./square');
 const Office = require('./Office');
+/*
+  Office 추가
+  */
 
 rtm.on('message', (message) => {
   const { channel } = message;
   const { text } = message;
 
-  if (!isNaN(text)) {
+  if (text === null) {
     square(rtm, text, channel);
   } else {
     switch (text) {
       case 'hi':
         greeting(rtm, channel);
+        break;
+      case '컴퓨터공학부':
+        Office(rtm, channel);
         break;
       default:
         rtm.sendMessage('I`m alive', channel);
