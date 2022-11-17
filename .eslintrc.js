47649aa3a53fdf7bd2df34bb3709be63905ddc97
hyein0229 <l1253@naver.com>
2022-11-17 20:00:23 +0900

    setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    env: {
-      es2021: true,
-      node: true,
-      mocha: true,
-    },
-    extends: ['airbnb-base'],
-    overrides: [
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest',
-    },
-    rules: {
-      // 'no-undef': 'off',
-      'no-return-assign': 'off',
-    },
-  };
+  env: {
+    es2021: true,
+    node: true,
+    mocha: true,
+  },
+  extends: ['airbnb-base'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    // 'no-undef': 'off',
+    'no-return-assign': 'off',
+  },
+};
