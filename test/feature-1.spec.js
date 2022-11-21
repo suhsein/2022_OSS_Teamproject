23336f934d13fe0891f6554a54123ba5455a41e7
hyein0229 <l1253@naver.com>
2022-11-21 22:18:46 +0900

    feat : feature1 테스트 코드 추가
    - 랜덤 인사 모듈 테스트

--- test/feature-1.spec.js
+++ test/feature-1.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable */s
+
 require('dotenv').config();
 
 const { RTMClient } = require('@slack/rtm-api');
@@ -19,7 +21,7 @@ console.log(token);
 const rtm = new RTMClient(token);
 rtm.start();
 
-const greeting = require('../greeting');
+const greeting = require('./greeting');
 
 let res;
 
@@ -28,7 +30,7 @@ describe('인사 모듈 테스트', () => {
   beforeEach(async () => res = await greeting(rtm, channel));
 
   describe('랜덤으로 인사를 합니다.', () => {
-    for (let i = 0; i < 10; i += 1) {
+    for (let i = 0; i < 5; i += 1) {
       it('Test - greeting() should return 3 random greeting pattern', (done) => {
         if (res === '1번 패턴 인사 성공' || res === '2번 패턴 인사 성공' || res === '3번 패턴 인사 성공') {
           console.log(res);
