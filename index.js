2571183d18d24dd159fbf467315706d0f42aa61b
hyein0229 <l1253@naver.com>
2022-11-22 03:15:42 +0900

    feat : 날짜 범위 체크하는 함수 추가
    - 학사일정 안내 함수 실행 전 날짜양식이 맞는지 확인할 때 범위를 체크하는 기능

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ rtm.start();
 const greeting = require('./greeting');
 const square = require('./square');
 const schedule = require('./schedule');
+
+function dateExist(text) {
+  const m1 = [1, 3, 5, 7, 8, 10, 12];
+  const m2 = [4, 6, 9, 11];
+
+  const month = Number(text.split('/')[0]);
+  const day = Number(text.split('/')[1]);
+
+  if (m1.includes(month) && day >= 1 && day <= 31) {
+    return true;
+  } if (m2.includes(month) && day >= 1 && day <= 30) {
+    return true;
+  } if (month === 2 && day >= 1 && day <= 29) {
+    return true;
+  }
+  return false;
+}
 /* eslint no-restricted-globals: 0 */
 const dateRegex = /\d{1,2}\/\d{1,2}/; // 월/일
 let state = 0;
@@ -29,14 +46,14 @@ rtm.on('message', (message) => {
 
   if (!isNaN(text)) {
     square(rtm, text, channel);
-    if (state === 1) { state = 0; }
-  } else if (dateRegex.test(text)) {
+    state = 0;
+  } else if (dateRegex.test(text) && dateExist(text)) {
     schedule(rtm, state, text, channel);
     if (state === 1) {
       state = 0;
     }
   } else {
-    if (state === 1) { state = 0; } // 학사일정 입력 후 날짜양식을 바로 입력하지 않으면 초기화
+    state = 0; // 학사일정 입력 후 날짜양식을 바로 입력하지 않으면 초기화
 
     switch (text) {
       case '테스트를 시작한다.':
