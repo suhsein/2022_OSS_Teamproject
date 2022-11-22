49de8cedf303d9a47c4c463c8c4943cdf473f485
hyein0229 <l1253@naver.com>
2022-11-22 18:11:35 +0900

    feat : spec.js 수정
    - 파일이 test 폴더 아래에 있으므로 모듈을 가져올 때 ./ -> ../ 로 변경

--- test/feature-1.spec.js
+++ test/feature-1.spec.js
@@ -21,7 +21,7 @@ console.log(token);
 const rtm = new RTMClient(token);
 rtm.start();
 
-const greeting = require('./greeting');
+const greeting = require('../greeting');
 
 let res;
 
--- test/feature-2.spec.js
+++ test/feature-2.spec.js
@@ -23,7 +23,7 @@ rtm.start();
 /* eslint import/no-unresolved: ["off"] */
 /* eslint import/extensions: ["off"] */
 const assert = require('assert');
-const schedule = require('./schedule');
+const schedule = require('../schedule');
 
 let res1;
 let res2;
