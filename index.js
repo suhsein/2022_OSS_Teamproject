842b75be6198f6f8283847c034947cd2215b0400
suhsein <76998096+suhsein@users.noreply.github.com>
2022-11-23 14:27:13 +0900

    feat 2: 스케줄 딕셔너리 모듈로 분리 (#41)
    * feat : 스케줄 딕셔너리 모듈로 분리
    
    슬랙봇 실행 시 스케줄을 딕셔너리로 만듦-학사일정 탐색 시 for문을
    반복해서 돌지 않도록 함-딕셔너리로 바로 검색 가능-날짜 양식 틀린 경우
    상태 추가
    
    * feat : 날짜 양식 틀릴 시 기능 수정
    
    학사일정을 입력하지 않고 날짜 양식이 틀린 경우,
    무시하도록 (i'm alive 응답)으로 수정
    
    Co-authored-by: suhsein <mini9075@gmail.com>

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ rtm.start();
 
 const greeting = require('./greeting');
 const square = require('./square');
-const schedule = require('./schedule');
+const schedule = require('./schedule'); // 학사일정 안내 모듈
+const getschedule = require('./getschedule'); // 학사일정 딕셔너리 생성 모듈. 학사일정 안내 시 매번 for문을 돌지 않도록 함.
 
 function dateExist(text) {
   const m1 = [1, 3, 5, 7, 8, 10, 12];
@@ -39,6 +40,7 @@ function dateExist(text) {
 /* eslint no-restricted-globals: 0 */
 const dateRegex = /\d{1,2}\/\d{1,2}/; // 월/일
 let state = 0;
+const scheduledict = getschedule(); // 학사일정 딕셔너리 가져오기
 
 rtm.on('message', (message) => {
   const { channel } = message;
@@ -47,11 +49,15 @@ rtm.on('message', (message) => {
   if (!isNaN(text)) {
     square(rtm, text, channel);
     state = 0;
-  } else if (dateRegex.test(text) && dateExist(text)) {
-    schedule(rtm, state, text, channel);
-    if (state === 1) {
-      state = 0;
+  } else if (state === 0 && dateRegex.test(text) && dateExist(text)) {
+    // 날짜 양식과 부합하나 학사일정을 먼저 입력하지 않은 경우
+    schedule(rtm, state, text, channel, scheduledict);
+  } else if (state === 1) {
+    if (!dateRegex.test(text) || !dateExist(text)) { // 날짜 양식이 틀렸거나 존재하지 않는 날짜의 경우
+      state = 2;
     }
+    schedule(rtm, state, text, channel, scheduledict);
+    state = 0;
   } else {
     state = 0; // 학사일정 입력 후 날짜양식을 바로 입력하지 않으면 초기화
 
@@ -63,7 +69,7 @@ rtm.on('message', (message) => {
         break;
       case '학사일정':
         schedule(rtm, state, text, channel);
-        state = 1;
+        state = 1; // 학사일정을 먼저 입력한 경우
         break;
       default:
         rtm.sendMessage('I`m alive', channel);
--- schedule.js
+++ schedule.js
@@ -1,28 +1,38 @@
-/* eslint no-unused-vars: off */
-
-function findSchedule(text) {
-  let schedule;
-  return schedule;
-}
-
-const schedule = function (rtm, state, text, channel) {
+const schedule = function (rtm, state, text, channel, scheduledict) {
   if (text === '학사일정') {
     console.log('학사일정을 안내합니다.');
     rtm.sendMessage('안내 받을 날짜를 이야기해주세요. (예, 12/21)', channel);
     return Promise.resolve('안내 메세지 출력');
   }
 
+  if (state === 2) { // set = 2 날짜 양식이 틀렸거나 존재하지 않는 날짜의 경우 에러 메세지 출력
+    console.log('잘못된 날짜 양식입니다.');
+    rtm.sendMessage('날짜 양식이 틀리거나 존재하지 않는 날짜입니다.', channel);
+    return Promise.resolve('잘못된 날짜 양식');
+  }
+
   if (state === 1) { // set = 1 (즉, 학사일정이 먼저 입력된 경우)만 날짜에 대한 학사일정 안내하기
-    // const scheduledict = getschedule(); // 학사일정을 저장한 딕셔너리 가져오기
-    const schedules = findSchedule(text);
-    if (schedules !== '') { // 딕셔너리에 있는 날짜라면
+    if (text in scheduledict) { // 딕셔너리에 있는 날짜라면
+      let schedules = '';
+      scheduledict[text].forEach((sche) => { // 해당 날짜의 모든 학사일정
+        if (schedules === '') {
+          schedules += sche;
+        } else {
+          schedules += `, ${sche}`;
+        }
+      });
+      console.log('학사일정 안내 성공');
       rtm.sendMessage(`${text}는 ${schedules} 입니다.`, channel); // 슬랫봇으로 학사일정 안내
-      return Promise.resolve(schedules);
+      return Promise.resolve('학사일정 안내 성공');
     }
+    console.log('학사일정 없음');
     rtm.sendMessage('해당 날짜의 학사일정이 없습니다.', channel);
     return Promise.resolve('학사일정 없음');
-  } // set = 0 인데 날짜 양식이 먼저 입력된 경우 접근 막기
-  rtm.sendMessage('[학사일정] 을 입력하시오.', channel);
+  }
+  // set = 0 인데 날짜 양식이 먼저 입력된 경우 접근 막기
+  console.log('학사일정 안내 실패');
+  rtm.sendMessage('[학사일정] 을 입력하십시오.', channel);
   return Promise.resolve('학사일정 안내 실패');
 };
+
 module.exports = schedule;
