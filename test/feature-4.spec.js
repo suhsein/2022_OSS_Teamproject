2250d7222c204db74c4da96129c576966536f2d1
suhsein <mini9075@gmail.com>
2022-11-24 18:39:25 +0900

    feat : Feature/4 기능 수정
    기존의 기능에서 학과 이름을
    대소문자 관련 없이 입력받을 수 있도록 수정

--- test/feature-4.spec.js
+++ test/feature-4.spec.js
@@ -17,14 +17,16 @@ rtm.start();
 
 const assert = require('assert');
 const office = require('../Office');
+const spaceRegex = / /gi;
 
 const testDict = {
-  'Computer Science and Engineering': 'College of Engineering Building 7, 224',
-  'Korean Language and Literature': 'College of Humanities, 320'
+  'computerscienceandengineering' : 'College of Engineering Building 7, 224',
+  'koreanlanguageandliterature' : 'College of Humanities, 320'
 }
 
 describe('전북대 학과 사무실 안내 모듈 테스트', () => {
-  for (var major in testDict) {
+  var major = major.replace(spaceRegex, '').toLowerCase();
+  for (major in testDict) {
     it('Test - 학과 입력시 해당 사무실 위치 반환', (done) => {
       assert.equal(office(rtm, major, channel, testDict), testDict[major]);
       done();
