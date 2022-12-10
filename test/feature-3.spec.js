83feb341872fe894db1f7e4e20e903be62f2eb5b
hyein0229 <l1253@naver.com>
2022-12-10 22:41:49 +0900

    feat : feature 3 유닛테스트 추가
    - 오늘의 메뉴 안내 및 평가 테스트
    - 주간 식단 평가 결과 안내 테스트

--- test/feature-3.spec.js
+++ test/feature-3.spec.js
@@ -1,11 +1,8 @@
-/* eslint-disable */
-
 require('dotenv').config();
 
 const { RTMClient } = require('@slack/rtm-api');
 
 const fs = require('fs');
-
 
 const channel = 'C046PKBTVTR';
 
@@ -23,15 +20,32 @@ const rtm = new RTMClient(token);
 rtm.start();
 
 const assert = require('assert');
+const getTodayMenu = require('../getTodayMenu');
+const getWeekStar = require('../getWeekStar');
 
-describe('진수원 식단 안내 및 평가 모듈 테스트', () => {
-    function randomTest(x) {
-      it(`Test - 난수 생성 시 ${x}이 나왔을 때 ${x+1}번 인사 패턴 출력`, function() {
-        assert.equal(greeting(rtm, channel, x), `${x+1}번 패턴 인사 성공`);
-      });
-    }
-  
-    for (let x = 0; x <= 2; x++) {
-      randomTest(x);
-    }
+let res1;
+let res2;
+let res;
+
+describe('오늘의 메뉴 안내 및 평가 모듈 테스트', () => {
+  before(async () => res1 = await getTodayMenu(rtm, 3, channel));
+  it('Test - 수요일에 해당하는 식단 안내', (done) => {
+    assert.equal(res1, '오늘의 식단메뉴 안내와 평가 성공');
+    done();
   });
+
+  before(async () => res2 = await getTodayMenu(rtm, 6, channel));
+  it('Test - 주말 예외', (done) => {
+    assert.equal(res2, '주말 예외처리');
+    done();
+  });
+
+  describe('주간 식단 평가 결과 테스트', function () {
+    this.timeout(7000);
+    before(async () => res = await getWeekStar(rtm, channel));
+    it('Test - 주간 식단 평가', (done) => {
+      assert.equal(res, '이번주 메뉴 평가 성공');
+      done();
+    });
+  });
+});
